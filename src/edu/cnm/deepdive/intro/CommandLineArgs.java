08fdb6a072a1c580c8afed7dd4eafecb3b926959
anakissler <fhresak@gmail.com>
2017-09-25 13:53:41 -0600

    Iteration exploration.

---
+++ src/edu/cnm/deepdive/intro/CommandLineArgs.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.intro;
+
+/**
+ * This class demonstrates iteration over command line arguments.
+ *
+ * @author Ana Kistner
+ */
+public class CommandLineArgs {
+
+  private static final String ARGUMENT_FORMAT = "Argument %d = %s%n";
+
+
+  /**
+   * Iterates through the arguments <code>args</code>, printing each one.
+   *
+   * @param args  command line arguments
+   */
+  public static void main(String[] args) {
+    for (int i = args.length - 1; i >= 0; i--) {
+      String argument = args[i];
+      System.out.printf(ARGUMENT_FORMAT, i, argument);
+    }
+  }
+}
